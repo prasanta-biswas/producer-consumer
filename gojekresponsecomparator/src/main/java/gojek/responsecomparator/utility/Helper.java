62d89a8b3b55f220adcf85fdcb7d04e11df33b3f
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-15 23:33:25 +0530

    fixed response format due to test failure

--- gojekresponsecomparator/src/main/java/gojek/responsecomparator/utility/Helper.java
+++ gojekresponsecomparator/src/main/java/gojek/responsecomparator/utility/Helper.java
@@ -1,9 +1,24 @@
 package gojek.responsecomparator.utility;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -36,9 +51,56 @@ public class Helper {
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
             }
-            return IOUtils.toString(connection.getInputStream());
+            String response = prettifyResponse(IOUtils.toString(connection.getInputStream()));
+            return response;
         } finally {
             connection.disconnect();
         }
+    }
+
+    private String prettifyResponse(String response){
+        try {
+            String result = prettifyJson(response);
+            return result;
+        }
+        catch (Exception e){
+
+        }
+
+        try {
+            String result = prettifyXml(response);
+            return result;
+        }
+        catch (Exception e){
+
+        }
+
+        return response;
+
+    }
+
+    private String prettifyJson(String jsonString){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(jsonString);
+        String formattedJson = gson.toJson(jsonElement);
+        return formattedJson;
+    }
+
+    private String prettifyXml(String xml) throws ParserConfigurationException, IOException, SAXException {
+
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = db.parse(new InputSource(new StringReader(xml)));
+
+        OutputFormat format = new OutputFormat(doc);
+        format.setIndenting(true);
+        format.setIndent(2);
+        format.setOmitXMLDeclaration(false);
+        format.setLineWidth(Integer.MAX_VALUE);
+        Writer outxml = new StringWriter();
+        XMLSerializer serializer = new XMLSerializer(outxml, format);
+        serializer.serialize(doc);
+
+        return outxml.toString();
     }
 }
