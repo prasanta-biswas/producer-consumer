2e5a55dc9ca9b3e85a62feed67825b004a7a4bee
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-16 00:45:43 +0530

    added main method for dummy test

--- gojekresponsecomparator/src/main/java/Main.java
+++ gojekresponsecomparator/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import gojek.responsecomparator.implementation.Comparator;
+import gojek.responsecomparator.specification.IComparator;
+
 /**
  * Created by prasantabiswas on 15/09/18.
  */
 public class Main {
+
+    private static final IComparator<String, String> COMPARATOR = new Comparator();
+    public static void main(String [] args){
+        COMPARATOR.getData("a","v");
+        COMPARATOR.getData("/Users/prasantabiswas/Desktop/file1.txt", "/Users/prasantabiswas/Desktop/file2.txt");
+    }
+
 }
