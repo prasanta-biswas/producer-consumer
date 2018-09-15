00b4da961a97051acf4e954dc8ddccf38d9d563d
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-15 23:07:45 +0530

    implemented getData method

--- gojekresponsecomparator/src/main/java/gojek/responsecomparator/implementation/Comparator.java
+++ gojekresponsecomparator/src/main/java/gojek/responsecomparator/implementation/Comparator.java
@@ -1,16 +1,53 @@
 package gojek.responsecomparator.implementation;
 
 import gojek.responsecomparator.specification.IComparator;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by prasantabiswas on 15/09/18.
  */
 public class Comparator implements IComparator<String,String>{
+    private File file1;
+    private File file2;
+    boolean isEqual;
     public boolean compare(String url1, String url2) {
         return false;
     }
 
     public void getData(String filePath1, String filePath2) {
+        this.file1 = new File(filePath1);
+        this.file2 = new File(filePath2);
+
+        LineIterator it1 = null;
+        LineIterator it2 = null;
+        try {
+            it1 = FileUtils.lineIterator(file1, "UTF-8");
+            it2 = FileUtils.lineIterator(file2, "UTF-8");
+
+            while (it1.hasNext() && it2.hasNext()) {
+                String line1 = it1.nextLine();
+                String line2 = it2.nextLine();
+                if(compare(line1,line2)) {
+                    System.out.println(line1 + " equals " + line2);
+                    isEqual = true;
+                }
+                else {
+                    System.out.println(line1 + " not equals " + line2);
+                    isEqual = false;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred: "+e);
+        } finally {
+            if(it1 != null)
+                LineIterator.closeQuietly(it1);
+            if(it2 != null)
+                LineIterator.closeQuietly(it2);
+        }
 
     }
 }
