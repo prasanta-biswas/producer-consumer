014c38905bfb02042f057142d20b356e34fffb1b
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-15 23:19:01 +0530

    added utility class for http request response

---
+++ gojekresponsecomparator/src/main/java/gojek/responsecomparator/utility/Helper.java
@@ -0,0 +1,44 @@
+package gojek.responsecomparator.utility;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by prasantabiswas on 15/09/18.
+ */
+public class Helper {
+
+    private static final Helper instance = new Helper();
+
+    private Helper() {
+
+    }
+
+    public static Helper getInstance(){
+        return instance;
+    }
+
+    public String getResponse(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection;
+        if(urlString.toLowerCase().contains("https"))
+            connection = (HttpsURLConnection) url.openConnection();
+        else
+            connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "text/plain");
+        connection.setRequestProperty("User-Agent", "chrome");
+        try{
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
+            }
+            return IOUtils.toString(connection.getInputStream()).replaceAll("\b", "").replaceAll("\n", "");
+        } finally {
+            connection.disconnect();
+        }
+    }
+}
