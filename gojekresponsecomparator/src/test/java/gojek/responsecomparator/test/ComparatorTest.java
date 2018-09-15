f6af8faeb61703b9a202774830d14532fdf390f1
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-15 22:53:55 +0530

    added 1 test case

--- gojekresponsecomparator/src/test/java/gojek/responsecomparator/test/ComparatorTest.java
+++ gojekresponsecomparator/src/test/java/gojek/responsecomparator/test/ComparatorTest.java
@@ -1,5 +1,8 @@
 package gojek.responsecomparator.test;
 
+import gojek.responsecomparator.implementation.Comparator;
+import gojek.responsecomparator.specification.IComparator;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,7 +12,8 @@ public class ComparatorTest {
 
     @Test
     public void testCompare(){
-
+        IComparator comparator = new Comparator();
+        Assert.assertTrue(comparator.compare("a","b"));
     }
 
     @Test
