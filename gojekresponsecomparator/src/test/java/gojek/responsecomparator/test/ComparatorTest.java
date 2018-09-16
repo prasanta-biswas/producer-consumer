c177e80516885b97f9864d6e6b8a54c4620f0221
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-16 20:34:41 +0530

    removed unnecessary code

--- gojekresponsecomparator/src/test/java/gojek/responsecomparator/test/ComparatorTest.java
+++ gojekresponsecomparator/src/test/java/gojek/responsecomparator/test/ComparatorTest.java
@@ -5,6 +5,8 @@ import gojek.responsecomparator.specification.IComparator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Created by prasantabiswas on 15/09/18.
  */
@@ -20,20 +22,6 @@ public class ComparatorTest {
     public void testCompareInvalidUrl(){
         IComparator comparator = new Comparator();
         Assert.assertFalse(comparator.compare("htt://reqres.in/api/users/1","htps://reqres.in/api/users/1"));
-    }
-
-    @Test
-    public void testGetDataWithInvalidData(){
-        Comparator comparator = new Comparator();
-        comparator.getData("abc.txt","xyz.txt");
-        Assert.assertFalse(comparator.isEqual());
-    }
-
-    @Test
-    public void testGetDataWithValidData(){
-        Comparator comparator = new Comparator();
-        comparator.getData("src/test/resources/file1.txt","src/test/resources/fil2.txt");
-        Assert.assertTrue(comparator.isEqual());
     }
 
 }
