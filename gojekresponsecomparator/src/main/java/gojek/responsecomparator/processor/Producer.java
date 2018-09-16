bc25f968f5f9fd11f92d0e07460db1345b4a976a
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-16 16:59:41 +0530

    replaced LineIterator with Scanner

--- gojekresponsecomparator/src/main/java/gojek/responsecomparator/processor/Producer.java
+++ gojekresponsecomparator/src/main/java/gojek/responsecomparator/processor/Producer.java
@@ -1,11 +1,9 @@
 package gojek.responsecomparator.processor;
 
 import gojek.responsecomparator.implementation.Comparator;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -25,25 +23,25 @@ public class Producer implements Runnable{
 
     @Override
     public void run() {
-        LineIterator it1 = null;
-        LineIterator it2 = null;
+        Scanner fileReader1 = null;
+        Scanner filerReader2 = null;
         try {
-            it1 = FileUtils.lineIterator(file1, "UTF-8");
-            it2 = FileUtils.lineIterator(file2, "UTF-8");
+            fileReader1 = new Scanner(file1, "UTF-8");
+            filerReader2 = new Scanner(file2, "UTF-8");
 
-            while (it1.hasNext() && it2.hasNext()) {
+            while (fileReader1.hasNextLine() && filerReader2.hasNextLine()) {
                 Comparator comparator = new Comparator();
-                comparator.setUrl1(it1.nextLine().trim());
-                comparator.setUrl2(it2.nextLine().trim());
+                comparator.setUrl1(fileReader1.nextLine().trim());
+                comparator.setUrl2(filerReader2.nextLine().trim());
                 queue.put(comparator);
             }
         } catch (InterruptedException | IOException e) {
             System.out.println("Error occurred: "+e);
         } finally {
-            if(it1 != null)
-                LineIterator.closeQuietly(it1);
-            if(it2 != null)
-                LineIterator.closeQuietly(it2);
+            if(fileReader1 != null)
+                fileReader1.close();
+            if(filerReader2 != null)
+                filerReader2.close();
         }
     }
 }
