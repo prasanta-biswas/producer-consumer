042793cd806f782aa033457da43357e2b7d9de97
Prasanta Biswas <prasanta.biswas@go-jek.com>
2019-10-02 14:13:01 +0530

    moved json processor to json handler

--- src/main/java/gojek/responsecomparator/handlers/JsonHandler.java
+++ src/main/java/gojek/responsecomparator/handlers/JsonHandler.java
@@ -1,52 +1,17 @@
 package gojek.responsecomparator.handlers;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import io.restassured.response.Response;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
 
 import java.util.*;
 
 public class JsonHandler {
 
-    static JsonParser parser = new JsonParser();
-
-    public static HashMap<String, Object> createHashMapFromJsonString(String json) {
-
-        JsonObject object = (JsonObject) parser.parse(json);
-        Set<Map.Entry<String, JsonElement>> set = object.entrySet();
-        Iterator<Map.Entry<String, JsonElement>> iterator = set.iterator();
-        HashMap<String, Object> map = new HashMap<String, Object>();
-
-        while (iterator.hasNext()) {
-
-            Map.Entry<String, JsonElement> entry = iterator.next();
-            String key = entry.getKey();
-            JsonElement value = entry.getValue();
-
-            if (null != value) {
-                if (!value.isJsonPrimitive()) {
-                    if (value.isJsonObject()) {
-
-                        map.put(key, createHashMapFromJsonString(value.toString()));
-                    } else if (value.isJsonArray() && value.toString().contains(":")) {
-
-                        List<HashMap<String, Object>> list = new ArrayList<>();
-                        JsonArray array = value.getAsJsonArray();
-                        if (null != array) {
-                            for (JsonElement element : array) {
-                                list.add(createHashMapFromJsonString(element.toString()));
-                            }
-                            map.put(key, list);
-                        }
-                    } else if (value.isJsonArray() && !value.toString().contains(":")) {
-                        map.put(key, value.getAsJsonArray());
-                    }
-                } else {
-                    map.put(key, value.getAsString());
-                }
-            }
-        }
-        return map;
+    public static boolean compareJson(Response jsonResponse1, Response jsonResponse2) throws JSONException {
+        JSONCompareResult result = JSONCompare.compareJSON(jsonResponse1.asString(),jsonResponse2.asString(), JSONCompareMode.STRICT);
+        return result.passed();
     }
 }
