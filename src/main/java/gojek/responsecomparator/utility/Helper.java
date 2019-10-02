d173ce36d1a29ba2020cbe98b8d97199445efc32
Prasanta Biswas <prasanta.biswas@go-jek.com>
2019-10-02 14:15:27 +0530

    replaced java http connection with restassured

--- src/main/java/gojek/responsecomparator/utility/Helper.java
+++ src/main/java/gojek/responsecomparator/utility/Helper.java
@@ -1,11 +1,10 @@
 package gojek.responsecomparator.utility;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by prasantabiswas on 15/09/18.
@@ -22,33 +21,9 @@ public class Helper {
         return instance;
     }
 
-    public String getResponse(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection;
-        if(urlString.toLowerCase().contains("https"))
-            connection = (HttpsURLConnection) url.openConnection();
-        else
-            connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Accept", "text/plain");
-        connection.setRequestProperty("User-Agent", "chrome");
-        try{
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
-            }
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            String response = "";
-            while ((inputLine = in.readLine()) != null)
-            {
-                response = response + inputLine;
-            }
-            in.close();
-            response = response.replaceAll("\n","").replaceAll("\r","")
-                    .replaceAll("\t","").replaceAll("\b","");
-            return response.toString();
-        } finally {
-            connection.disconnect();
-        }
+    public Response getResponse(String urlString) throws IOException {
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get(urlString);
+        return response;
     }
 }
